91be33c2e28cf6cebda8aa4eea63ef179361b45e
Van Celdran <celdranvan@gmail.com>
2023-09-12 19:24:45 +0800

    initialize main page

---
+++ app/main-page/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '@components/Footer';
+const Main = () => {
+  return (
+   <div className='min-h-full bg-black'>
+    <div className="relative h-[200vh] md:bg-center sm:bg-right bg-[600px] bg-cover flex items-center justify-center p-6 bg-black text-white">Main</div>
+  <Footer/>
+  </div>
+    )
+}
+
+export default Main
